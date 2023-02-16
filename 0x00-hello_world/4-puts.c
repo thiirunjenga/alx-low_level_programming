f732b1e40f49c9b44b200daaf8e576ea707093d5
thiirunjenga <thiiru.n@gmail.com>
2023-02-16 15:44:28 -0800

    print on new line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
  *Description: main - prints out a string to stdout.
  *Return: 0 if success.
  */
+
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle");
-return (0);
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
 }
